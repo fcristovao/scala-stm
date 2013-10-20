4df5efa9d689bdc962f97f7b285fe6eeb9b9c7ff
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2013-10-20 15:09:35 +0200

    Added Stub for InTxnEnd.

---
+++ src/main/scala/scala/concurrent/stm/stubs/StubInTxnEnd.scala
@@ -0,0 +1,23 @@
+package scala.concurrent.stm.stubs
+
+import scala.concurrent.stm._
+
+trait StubInTxnEnd extends InTxnEnd{
+	import scala.concurrent.stm.Txn._
+
+  // The user-visible versions of these methods are in the Txn object.
+
+  def status: Status = throw new AbstractMethodError
+  def rootLevel: NestingLevel = throw new AbstractMethodError
+  def currentLevel: NestingLevel = throw new AbstractMethodError
+  def rollback(cause: RollbackCause): Nothing = throw new AbstractMethodError
+  def retry(): Nothing = throw new AbstractMethodError
+  def retryFor(timeoutNanos: Long) = throw new AbstractMethodError
+  def beforeCommit(handler: InTxn => Unit) = throw new AbstractMethodError
+  def whilePreparing(handler: InTxnEnd => Unit) = throw new AbstractMethodError
+  def whileCommitting(handler: InTxnEnd => Unit) = throw new AbstractMethodError
+  def afterCommit(handler: Status => Unit) = throw new AbstractMethodError
+  def afterRollback(handler: Status => Unit) = throw new AbstractMethodError
+  def afterCompletion(handler: Status => Unit) = throw new AbstractMethodError
+  def setExternalDecider(decider: ExternalDecider) = throw new AbstractMethodError
+}
