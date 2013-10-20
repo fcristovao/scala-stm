8277dd6dd3c3774109ff4870dcdd1d83fb0beef1
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2013-10-20 15:05:14 +0200

    minor edit: Just identation things

--- src/main/scala/scala/concurrent/stm/stubs/StubRef.scala
+++ src/main/scala/scala/concurrent/stm/stubs/StubRef.scala
@@ -7,19 +7,19 @@ class StubRef[A] extends Ref[A] {
 	
 	//from SourceLike[A]:
 	def get(implicit txn: InTxn): A = throw new AbstractMethodError
-  def getWith[Z](f: (A) => Z)(implicit txn: InTxn): Z = throw new AbstractMethodError
-  def relaxedGet(equiv: (A, A) => Boolean)(implicit txn: InTxn): A = throw new AbstractMethodError 
-  
-  //from SinkLike[A]
-  def set(v: A)(implicit txn: InTxn) = throw new AbstractMethodError
-  def trySet(v: A)(implicit txn: InTxn): Boolean = throw new AbstractMethodError
+	def getWith[Z](f: (A) => Z)(implicit txn: InTxn): Z = throw new AbstractMethodError
+	def relaxedGet(equiv: (A, A) => Boolean)(implicit txn: InTxn): A = throw new AbstractMethodError 
+	
+	//from SinkLike[A]
+	def set(v: A)(implicit txn: InTxn) = throw new AbstractMethodError
+	def trySet(v: A)(implicit txn: InTxn): Boolean = throw new AbstractMethodError
+	
+	//from RefLike[A]
+	def swap(v: A)(implicit txn: InTxn): A = throw new AbstractMethodError
+	def transform(f: A => A)(implicit txn: InTxn) = throw new AbstractMethodError
+	def transformIfDefined(pf: PartialFunction[A,A])(implicit txn: InTxn): Boolean = throw new AbstractMethodError
+	
+	//from Ref[A]
+	def single: View[A] = throw new AbstractMethodError
 
-  //from RefLike[A]
-  def swap(v: A)(implicit txn: InTxn): A = throw new AbstractMethodError
-  def transform(f: A => A)(implicit txn: InTxn) = throw new AbstractMethodError
-  def transformIfDefined(pf: PartialFunction[A,A])(implicit txn: InTxn): Boolean = throw new AbstractMethodError
-  
-  //from Ref[A]
-  def single: View[A] = throw new AbstractMethodError
-  
 }
