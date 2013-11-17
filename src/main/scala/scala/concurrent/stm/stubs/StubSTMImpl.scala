cf254284f155aaf3ec53294a80ac77d7d4295c4b
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2013-11-17 12:57:06 +0100

    Formatting fixes

--- src/main/scala/scala/concurrent/stm/stubs/StubSTMImpl.scala
+++ src/main/scala/scala/concurrent/stm/stubs/StubSTMImpl.scala
@@ -4,7 +4,6 @@ import scala.concurrent.stm._
 import scala.concurrent.stm.impl.STMImpl
 import java.util.concurrent.TimeUnit
 
-trait StubSTMImpl extends STMImpl with StubRefFactory with StubTxnContext with StubTxnExecutor{
-
+trait StubSTMImpl extends STMImpl with StubRefFactory with StubTxnContext with StubTxnExecutor {
 	def newCommitBarrier(timeout: Long, unit: TimeUnit): CommitBarrier = throw new AbstractMethodError
 }
