5370c667ce615c2ba4d6959c1ecf429b6a3e6d5d
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2013-11-17 12:56:12 +0100

    Add sensible default to StubTxnContext

--- src/main/scala/scala/concurrent/stm/stubs/StubTxnContext.scala
+++ src/main/scala/scala/concurrent/stm/stubs/StubTxnContext.scala
@@ -4,6 +4,16 @@ import scala.concurrent.stm._
 import scala.concurrent.stm.impl.TxnContext
 
 trait StubTxnContext extends TxnContext {
-	def findCurrent(implicit mt: MaybeTxn): Option[InTxn] = throw new AbstractMethodError
+	def findCurrent(implicit mt: MaybeTxn): Option[InTxn] = {
+		mt match {
+			case TxnUnknown => {
+				dynCurrentOrNull match {
+					case null => None
+					case txn => Some(txn)
+				}
+			}
+			case txn: InTxn => Some(txn)
+		}
+	}
 	def dynCurrentOrNull: InTxn = throw new AbstractMethodError
 }
